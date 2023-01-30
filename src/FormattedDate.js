d44d7c741649b949b5992ea9d484babe30383571
Anastasiia Bilova <anastasiabilova@gmail.com>
2023-01-30 15:54:15 +0200

    Add constants

--- src/FormattedDate.js
+++ src/FormattedDate.js
@@ -1,25 +1,17 @@
 import React from "react";
 
+import { LIST_OF_EXTENDED_DAYS_OF_THE_WEEK } from "./Constants";
 import "./Weather.css";
 
 export default function FormattedDate(props) {
-  let extendedDays = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-
-  let hours = props.date.getHours();
-  let minutes = String(props.date.getMinutes()).padStart(2, "0");
-  let day = extendedDays[props.date.getDay()];
+  let currentDate = new Date(props.date * 1000);
+  let hours = currentDate.getHours();
+  let minutes = String(currentDate.getMinutes()).padStart(2, "0");
+  let day = LIST_OF_EXTENDED_DAYS_OF_THE_WEEK[currentDate.getDay()];
 
   return (
     <div className="FormattedDate">
-      <p className="lead mb-0">
+      <p className="lead fw-lighter mb-0">
         {day} {hours}:{minutes}
       </p>
     </div>
--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -1,26 +1,26 @@
 import React from "react";
 
 import "./WeatherForecastDay.css";
+import { LIST_OF_DAYS_OF_THE_WEEK } from "./Constants";
 import WeatherIcon from "./WeatherIcon";
 
 export default function WeatherForecastDay(props) {
-  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
   let date = new Date(props.data.dt * 1000);
-  let day = days[date.getDay()];
+  let day = LIST_OF_DAYS_OF_THE_WEEK[date.getDay()];
 
   return (
     <div className="WeatherForecastDay">
-      <div className="day">{day}</div>
+      <div className="day py-3">{day}</div>
       <WeatherIcon
         code={props.data.weather[0].icon}
-        size={44}
+        size={40}
         className="icon"
       />
-      <div className="temperature">
+      <div className="temperature pt-2">
         <span className="temperature-max">
           {Math.round(props.data.temp.max)}°
         </span>
-        <span className="temperature-min">
+        <span className="temperature-min ms-2">
           {Math.round(props.data.temp.min)}°
         </span>
       </div>
