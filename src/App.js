bc8924d2e3eeaaac0ec48e85828507d77f8bbf4f
Anastasiia Bilova <anastasiabilova@gmail.com>
2023-01-30 15:42:41 +0200

    Remove unused component video

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 
 import Footer from "./Footer";
-import Video from "./Video";
 import Weather from "./Weather";
 
 import "./App.css";
@@ -9,13 +8,10 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <section>
-        <Video />
-        <div className="container h-100">
-          <Weather defaultCity="London" />
-          <Footer />
-        </div>
-      </section>
+      <div className="container">
+        <Weather defaultCity="London" />
+        <Footer />
+      </div>
     </div>
   );
 }
