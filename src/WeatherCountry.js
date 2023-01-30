db974afdd6b0a8d32f95c0464b07e042d9219134
Anastasiia Bilova <anastasiabilova@gmail.com>
2023-01-30 16:19:53 +0200

    Add the useEffect hooks to get the list of countries and set the value of the country variable when the country code has been changed

---
+++ src/WeatherCountry.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+export default function WeatherCountry(props) {
+  const [country, setCountry] = useState("");
+  const [countryList, setCountryList] = useState({});
+
+  useEffect(() => {
+    async function fetchCountryList() {
+      const response = await axios.get(
+        `https://pkgstore.datahub.io/core/country-list/data_json/data/8c458f2d15d9f2119654b29ede6e45b8/data_json.json`
+      );
+      if (Object.keys(response).length !== 0) setCountryList(response.data);
+    }
+    fetchCountryList();
+  }, []);
+
+  useEffect(() => {
+    if (Object.keys(countryList).length !== 0) {
+      countryList.forEach((countryElement) => {
+        if (countryElement.Code === props.countryCode) {
+          setCountry(countryElement.Name);
+        }
+      });
+    }
+  }, [props.countryCode]);
+
+  return (
+    <div className="WeatherCountry">
+      <h1 className="display-6 fw-lighter">{country}</h1>
+    </div>
+  );
+}
